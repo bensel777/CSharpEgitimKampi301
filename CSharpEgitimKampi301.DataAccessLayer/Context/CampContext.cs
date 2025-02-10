62b29ce4660d4100bb8da923f126dedf5d005e87
Bensel <benselkorkmaz@gmail.com>
2025-02-10 18:56:47 +0000

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Context/CampContext.cs
@@ -0,0 +1,29 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Context
+{
+    public class CampContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
+
+/*
+   bütün sınıflar bunun içinde yer alıcak           
+ public class CampContext : DbContext -- > 
+    {   
+        public DbSet<Category>       Categories { get; set; }
+ c# tarafında kullanacağımız ismi  |   Sql'e yansıyacak olan ismi     
+
+birbirinden ayırt edilsin diye yalın hali class C# | Çoğul halini Sql de kullanılır...
+*/
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+
+/*
+ 
+Field-Variable-Property
+
+ */
+
+/*
+ int x; ---> Field --> alan class'ın direct içinde tanımlanıyorsa Field 
+*/
+
+/*
+ public int  y {get; set; } --> property --> alan
+*/
+
+/*
+ void test.()
+{
+    int z;
+}
+-----> Değişken -- > Variable
+*/ 
